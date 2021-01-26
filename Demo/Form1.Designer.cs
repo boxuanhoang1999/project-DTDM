c521b21b09bdb3375627f02e29870ed065db6b7a
boxuanhoang1999 <78013812+boxuanhoang1999@users.noreply.github.com>
2021-01-26 15:34:36 +0700

    Add files via upload

---
+++ Demo/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvdemo = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvdemo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvdemo
+            // 
+            this.dgvdemo.BackgroundColor = System.Drawing.Color.White;
+            this.dgvdemo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvdemo.Location = new System.Drawing.Point(12, 236);
+            this.dgvdemo.Name = "dgvdemo";
+            this.dgvdemo.RowHeadersWidth = 51;
+            this.dgvdemo.RowTemplate.Height = 24;
+            this.dgvdemo.Size = new System.Drawing.Size(1134, 405);
+            this.dgvdemo.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LimeGreen;
+            this.button1.Location = new System.Drawing.Point(23, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Yellow;
+            this.button2.Location = new System.Drawing.Point(23, 89);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 49);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Run";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(205, 30);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(620, 174);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Location = new System.Drawing.Point(23, 155);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(132, 49);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "NOC",
+            "region",
+            "notes"});
+            this.comboBox1.Location = new System.Drawing.Point(861, 37);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(285, 24);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.Tag = "Abc";
+            this.comboBox1.Text = "Thuộc tính bảng Region";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "ID",
+            "Name",
+            "Sex",
+            "Age",
+            "Height",
+            "Weight",
+            "Team",
+            "NOC",
+            "Gam",
+            "Year",
+            "Season",
+            "City",
+            "Sport",
+            "Event",
+            "Medal"});
+            this.comboBox2.Location = new System.Drawing.Point(861, 102);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(285, 24);
+            this.comboBox2.TabIndex = 6;
+            this.comboBox2.Text = "Thuộc tính bảng Athlete";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DisplayMember = "fdgdfg";
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Những VDV lớn tuổi nhất giành được HCV",
+            "Top 5 quốc gia giành nhiều huy chường vàng nhất",
+            "Top 10 quốc gia có nhiều vận động viên tham dự olympic"});
+            this.comboBox3.Location = new System.Drawing.Point(861, 168);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(285, 24);
+            this.comboBox3.TabIndex = 7;
+            this.comboBox3.Text = "Code mẫu ";
+            this.comboBox3.ValueMember = "fdgdfg";
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(380, 89);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(256, 476);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SandyBrown;
+            this.ClientSize = new System.Drawing.Size(1171, 653);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgvdemo);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvdemo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvdemo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Demo/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.ForeColor = Color.LightGray;
+            textBox1.Text = "Viết câu truy vấn";
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            
+        }
+        private void LoadData(string strFilePath)
+        {
+            DataTable dt = new DataTable();
+            string[] lines = System.IO.File.ReadAllLines(strFilePath);
+            if (lines.Length > 0)
+            {
+                //Header
+                string strfirstLine = lines[0];
+                string[] headerLabels = strfirstLine.Split(',');
+                foreach (string strheader in headerLabels)
+                {
+                    dt.Columns.Add(new DataColumn(strheader));
+                }
+                //Data
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    string[] strData = lines[i].Split(',');
+                    DataRow dr = dt.NewRow();
+                    int columnIndex = 0;
+                    foreach (string strheader in headerLabels)
+                    {
+                        dr[strheader] = strData[columnIndex++];
+                    }
+                    dt.Rows.Add(dr);
+                }
+            }
+            if (dt.Rows.Count > 0)
+            {
+                dgvdemo.DataSource = dt;
+            }
+        }
+        public string ExecuteCommandSync(object command)
+        {
+            try
+            {
+                System.Diagnostics.ProcessStartInfo procStartInfo = new System.Diagnostics.ProcessStartInfo();
+                procStartInfo.FileName = "cmd";
+                procStartInfo.Arguments = ""+command;
+                procStartInfo.RedirectStandardOutput = true;
+                procStartInfo.UseShellExecute = false;
+                procStartInfo.CreateNoWindow = true;
+                
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo = procStartInfo;
+                proc.Start();
+                string result = proc.StandardOutput.ReadToEnd();
+                return result;
+            }
+            catch (Exception objException)
+            {
+                return "";
+            }
+        }
+        public void ghifile(string a)
+        {
+            string b = @"hive -e 'use demo; "+a+";' | sed 's/[\\t]\\+/,/g' > output8.csv";
+            File.WriteAllText("E:\\DienToanDamMay\\CuoiKy\\docker-hive\\compile.sh", "cd ..\n" + b);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ghifile(textBox1.Text);
+
+            string command = @"/c ""docker cp E:\DienToanDamMay\CuoiKy\docker-hive\compile.sh 3b208d8ab7e5:compile.sh"" ";
+            ExecuteCommandSync(command);
+
+
+            string batFileName = @"E:\DienToanDamMay\CuoiKy\docker-hive" + @"\" + Guid.NewGuid() + ".bat";
+            using (StreamWriter batFile = new StreamWriter(batFileName))
+            {
+                batFile.WriteLine($"E:");
+                batFile.WriteLine($@"cd E:\DienToanDamMay\CuoiKy\docker-hive");
+                batFile.WriteLine($"docker-compose exec hive-server bash ../compile.sh");
+            }
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd.exe", "/c " + batFileName);
+            processStartInfo.UseShellExecute = false;
+            processStartInfo.CreateNoWindow = false;
+            processStartInfo.WindowStyle = ProcessWindowStyle.Normal;
+            Process p = new Process();
+            p.StartInfo = processStartInfo;
+            p.Start();
+            p.WaitForExit();
+            File.Delete(batFileName);
+
+
+            ExecuteCommandSync(@"/c ""docker cp docker-hive_hive-server_1:/output8.csv E:\DienToanDamMay\CuoiKy""");
+
+            LoadData("E:/DienToanDamMay/CuoiKy/output8.csv");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ExecuteCommandSync(@"/c ""docker stop 1a9 281 ca2 74e 3b2 64d""");
+            MessageBox.Show("Stop thành công !", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ExecuteCommandSync(@"/c ""docker start 1a9 281 ca2 74e 3b2 64d""");
+            MessageBox.Show("Start thành công !", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "Viết câu truy vấn";
+                textBox1.ForeColor = Color.Gray;
+            }
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Viết câu truy vấn")
+            {
+                textBox1.Text = "";
+                textBox1.ForeColor = Color.Black;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox3.SelectedItem == "Những VDV lớn tuổi nhất giành được HCV")
+            {
+                textBox1.ForeColor = Color.Black;
+                textBox1.Text = @"select distinct Name,tuoi,gam,team,sport from (select max(age) as tuoi from athlete where medal=""Gold"")a ,athlete b where a.tuoi=b.age";
+            }
+            if (comboBox3.SelectedItem == "Top 5 quốc gia giành nhiều huy chường vàng nhất")
+            {
+                textBox1.ForeColor = Color.Black;
+                textBox1.Text = @"select region,count(Name) as sl from athlete INNER JOIN region on(athlete.NOC= region.NOC) where Medal= ""Gold"" group by region order by sl desc limit 5";
+            }
+            if (comboBox3.SelectedItem == "Top 10 quốc gia có nhiều vận động viên tham dự olympic")
+            {
+                textBox1.ForeColor = Color.Black;
+                textBox1.Text = @"select region,count(Name) as sl from athlete INNER JOIN region on (athlete.noc=region.noc) group by region order by sl desc limit 10";
+            }
+           
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string batFileName = @"E:\DienToanDamMay\CuoiKy\docker-hive" + @"\" + Guid.NewGuid() + ".bat";
+            using (StreamWriter batFile = new StreamWriter(batFileName))
+            {
+                batFile.WriteLine($"C:");
+                batFile.WriteLine($@" C:\Users\HOME\anaconda3\Scripts\activate.bat C:\Users\HOME\anaconda3 & conda activate tensorflow1");
+                //batFile.WriteLine($"conda activate tensorflow1");
+                batFile.WriteLine($@"cd C:\Tensorflow2\workspace\training_demo");
+                batFile.WriteLine($"python Object_detection_image.py");
+            }
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd.exe", "/c " + batFileName);
+            processStartInfo.UseShellExecute = false;
+            processStartInfo.CreateNoWindow = false;
+            processStartInfo.WindowStyle = ProcessWindowStyle.Normal;
+            Process p = new Process();
+            p.StartInfo = processStartInfo;
+            p.Start();
+            p.WaitForExit();
+            File.Delete(batFileName);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var workingDirectory = Path.GetFullPath("Scripts");
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    RedirectStandardInput = true,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    WorkingDirectory = workingDirectory
+                }
+            };
+            process.Start();
+            process.WaitForExit();
+            // Pass multiple commands to cmd.exe
+            using (var sw = process.StandardInput)
+            {
+                if (sw.BaseStream.CanWrite)
+                {
+                    // Vital to activate Anaconda
+                    sw.WriteLine("C:\\Users\\HOME\\anaconda3\\Scripts\\activate.bat");
+                    // Activate your environment
+                    sw.WriteLine("activate tensorflow1");
+                    // Any other commands you want to run
+                    sw.WriteLine("cd C:\\Tensorflow2\\workspace\\training_demo");
+                    // run your script. You can also pass in arguments
+                    sw.WriteLine("python Object_detection_image.py");
+                }
+            }
+
+            // read multiple output lines
+            while (!process.StandardOutput.EndOfStream)
+            {
+                var line = process.StandardOutput.ReadLine();
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
